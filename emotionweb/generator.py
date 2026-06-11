ef7f9bc4ab125aba7181ab60a54b7f84d7987b59
unknown <aniketvishwa39@gmail.com>
2026-06-11 11:35:38 +0530

    Added project files

--- emotionweb/generator.py
+++ emotionweb/generator.py
@@ -1,5 +1,7 @@
 import json
 import webbrowser
+import shutil
+import os
 
 class Surprise:
 
@@ -8,26 +10,69 @@ class Surprise:
         type,
         name,
         message,
-        images
+        images,
+        caption,
     ):
 
         self.type = type
         self.name = name
         self.message = message
         self.images = images
+        self.caption= caption
 
     def generate(self):
 
+        upload_folder = (
+            "webengine/static/uploads"
+        )
+
+        os.makedirs(
+            upload_folder,
+            exist_ok=True
+        )
+
+        copied = []
+
+        for i,image in enumerate(self.images):
+
+            ext = image.split(".")[-1]
+
+            new_name = f"img{i+1}.{ext}"
+
+            shutil.copy(
+                image,
+                f"{upload_folder}/{new_name}"
+            )
+
+            copied.append(
+                new_name
+            )
+
         data = {
-            "type": self.type,
-            "name": self.name,
-            "message": self.message,
-            "images": self.images
+
+            "name":self.name,
+
+            "message":self.message,
+
+            "images":copied,
+            "caption":self.caption,
         }
 
-        with open("data.json", "w") as file:
-            json.dump(data, file, indent=4)
+        with open(
+            "data.json",
+            "w"
+        ) as file:
 
-        webbrowser.open("http://127.0.0.1:1214")
+            json.dump(
+                data,
+                file,
+                indent=4
+            )
 
-        print("Website generated successfully!")
+        webbrowser.open(
+            "http://127.0.0.1:1214"
+        )
+
+        print(
+            "Website generated successfully!"
+        )
--- test.py
+++ test.py
@@ -1,18 +1,42 @@
 from emotionweb import Surprise
 
 s = Surprise(
+
     type="love",
-    name="Aniket",
-    message="You are my world ❤️",
+
+    name="Aniket Vishwakarma",
+
+    message="India is the bestest palce to travel not for their beauty but also for their temples and ancient history  ❤️",
+
     images=[
-        "1.jpg",
-        "2.jpg",
-        "3.jpg",
-        "4.jpg",
-        "5.jpg",
-        "6.jpg",
-        "7.jpg",
-        "8.jpg"
+
+        "/home/kali/Desktop/hack/7.jpeg",
+        "/home/kali/Desktop/hack/8.jpeg",
+        "/home/kali/Desktop/hack/9.jpeg",
+        "/home/kali/Desktop/hack/10.jpeg",
+        "/home/kali/Desktop/hack/12.jpeg",
+        "/home/kali/Desktop/hack/13.jpeg",
+        "/home/kali/Desktop/hack/14.jpeg",
+        "/home/kali/Desktop/hack/15.jpeg"
+    ],
+
+    caption=[
+
+        "The solo travel that teaches me everything ❤️",
+
+        "This travel shows a different path of Bihar ❤️",
+
+        "And this one is closest to heart beacause its my varanasi yarr❤️",
+
+        "very best view ❤️",
+
+        "Trip Together ❤️",
+
+        "Fun Moments ❤️",
+
+        "Always Smiling ❤️",
+
+        "Forever ❤️"
     ]
 )
 
